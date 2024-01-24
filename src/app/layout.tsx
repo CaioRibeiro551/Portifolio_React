5f0f24a723350eb60dbb1ecc511d591e34d2cdeb
Caio <caio.h.r@outlook.com>
2024-01-24 15:31:50 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.scss";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Caio Ribeiro",
+  description: "Caio Ribeiro is a Software Developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
