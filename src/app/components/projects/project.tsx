f70d065d6431af721e83d3208943a34a265e8529
Caio <caio.h.r@outlook.com>
2024-02-09 16:30:05 -0300

    Melhorando responsividade e alinhamento

--- src/app/components/projects/project.tsx
+++ src/app/components/projects/project.tsx
@@ -1,37 +1,40 @@
 import './project.scss';
 export function Projects() {
   return (
-    <div className="container-projects">
-      <div className="project-contents">
-        <img id="project1" src="./pagar-me.png" alt="" />
-        <h3>Pagar-me</h3>
-        <p>
-          Plataforma para gerenciamento de pagamentos e clientes inadimplentes
-        </p>
-        <div className="btn-project">
-          <a href="">
-            <p>Ver projeto</p>
-          </a>
+    <div className="Father-content">
+      <h2>Projetos</h2>
+      <div className="container-projects">
+        <div className="project-contents">
+          <img id="project1" src="./pagar-me.png" alt="" />
+          <div className="project-contents">
+            <h3>Pagar-me</h3>
+            <p>Plataforma para gerenciamento de pagamentos</p>
+            <div className="btn-project">
+              <a href="">
+                <p>Ver projeto</p>
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="project-contents">
-        <img src="./Dindin.png" alt="" />
-        <h3>Dindin</h3>
-        <p>Gerenciador de finanças</p>
-        <div className="btn-project">
-          <a href="">
-            <p>Ver projeto</p>
-          </a>
+        <div className="project-contents">
+          <img src="./Dindin.png" alt="" />
+          <h3>Dindin</h3>
+          <p>Gerenciador de finanças</p>
+          <div className="btn-project">
+            <a href="">
+              <p>Ver projeto</p>
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="project-contents">
-        <img src="./spiderman.png" alt="" />
-        <h3>Spider-man | Multiverso</h3>
-        <p>Nostalgia filmes Spider-man</p>
-        <div className="btn-project">
-          <a href="">
-            <p>Ver projeto</p>
-          </a>
+        <div className="project-contents">
+          <img src="./spiderman.png" alt="" />
+          <h3>Spider-man | Multiverso</h3>
+          <p>Nostalgia filmes Spider-man</p>
+          <div className="btn-project">
+            <a href="">
+              <p>Ver projeto</p>
+            </a>
+          </div>
         </div>
       </div>
     </div>
